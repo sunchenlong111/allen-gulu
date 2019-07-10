9c5372f752fd870534e1e1fa2e256a0655867dd9
孙陈龙 <1094584419@qq.com>
2019-07-10 16:12:51 +0800

    终于搞好了这个操蛋的parcel

---
+++ README.md
@@ -0,0 +1,10 @@
+##一款基于Vue的ui框架
+
+
+
+##注意 要使用这款框架，需要在css中设置box-sizing:border-box
+
+
+作者：ALLEN
+
+
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button1 from './button'
+
+Vue.component('g-button',Button1)
+
+new Vue({
+    el:'#app',
+})
