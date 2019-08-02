7941005453f17a751ca0f4b8bd1e2eba90365386
孙陈龙 <1094584419@qq.com>
2019-08-02 10:43:07 +0800

    完善grid布局组件，补充相关单元测试文件

--- src/col.vue
+++ src/col.vue
@@ -31,16 +31,30 @@
             pc: {type: Object, validator,},
             widePc: {type: Object, validator,}
         },
+        methods: {
+            createClasses(obj, str = '') {
+                if (!obj) {
+                    return []
+                }
+                let arr = []
+                if (obj.span) {
+                    arr.push(`col-${str}${obj.span}`)
+                }
+                if (obj.offset) {
+                    arr.push(`offset-${str}${obj.offset}`)
+                }
+                return arr
+            }
+        },
         computed: {
             colClass() {
                 let {span, offset, ipad, narrowPc, pc, widePc} = this
                 return [
-                    span && `col-${span}`,
-                    offset && `offset-${offset}`,
-                    (ipad ? [`col-ipad-${ipad.span}`] : []),
-                    (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-                    (pc ? [`col-pc-${pc.span}`] : []),
-                    (widePc ? [`col-wide-pc-${widePc.span}`] : []),
+                    ...this.createClasses({span, offset}),
+                    ...this.createClasses(ipad,'ipad-'),
+                    ...this.createClasses(narrowPc,'narrow-pc-'),
+                    ...this.createClasses(pc,'pc-'),
+                    ...this.createClasses(widePc,'wide-pc-')
                 ]
             },
             colStyle() {
@@ -69,7 +83,7 @@
                 margin-left: ($n/24) * 100%;
             }
         }
-        @media(min-width: 577px) and (max-width: 768px) {
+        @media(min-width: 577px) {
             $class-prefix: col-ipad-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -83,7 +97,7 @@
                 }
             }
         }
-        @media(min-width: 769px) and (max-width: 992px) {
+        @media(min-width: 769px) {
             $class-prefix: col-narrow-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -97,7 +111,7 @@
                 }
             }
         }
-        @media(min-width: 993px) and (max-width: 1200px) {
+        @media(min-width: 993px) {
             $class-prefix: col-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -111,7 +125,7 @@
                 }
             }
         }
-        @media(min-width: 1201px)  {
+        @media(min-width: 1201px) {
             $class-prefix: col-wide-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
